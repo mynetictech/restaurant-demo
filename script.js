1cf7b82598cdf8faa3bbdc10c2b300d67d41e86a
mynetictech <mynetictech@gmail.com>
2026-01-01 23:46:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ showTime();
 setInterval(function () {
 	showTime();
 }, 1000);
+
+fetch('https://api.countapi.xyz/hit/mynetictech-royalspice/visits')
+.then(response => response.json())
+.then(data => {
+    document.getElementById('visitor-count').innerText = data.value;
+})
+.catch(() => {
+    document.getElementById('visitor-count').innerText = "N/A";
+});
